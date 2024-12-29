b3be8e3211bc4335ae669514940c6557305192dc
Tee <hungtruong2x3@gmail.com>
2024-12-29 10:34:54 +0700

    bus

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject gameOverScreen;
 
+    public void Awake()
+    {
+        gameOverScreen.SetActive(false);
+    }
+  
 
     public void GameOver()
     {
         gameOverScreen.SetActive(true);
 
     }
+    
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+
 }
